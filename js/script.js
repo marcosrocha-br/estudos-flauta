e2b889cd91a91cfe2f633f06fb145007de933abe
Marcos Rocha <marcosrocha.br@hotmail.com>
2021-12-04 18:50:19 -0300

    inicio - dados

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const TONALIDADE = {
+    escala:["C","D","E","F","G","A","B"], 
+    acidente: ["bemol", "sustenido"], 
+    modo: ["maior","menor"], 
+    altura: ["8ª acima", "8ª abaixo"]
+};
+   
+const DINAMICAS = ["pp", "p", "mf", "f", "ff"];
+
+const VELOCIDADE = ["muito lento", "lento", "normal", "rápido", "muito rápido"];
